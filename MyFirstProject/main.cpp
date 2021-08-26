ba9d134afd7a8d1599d05e52334cd3328d35e90b
Jayce Abens <jayce_abens@s.webster-city.k12.ia.us>
2021-08-26 10:37:50 -0500

    Update 1

--- MyFirstProject/main.cpp
+++ MyFirstProject/main.cpp
@@ -1,6 +1,15 @@
+/*
+	Jayce Abens
+	FA2021
+	Due: September 1, 2021
+	Lab 1 Introduction to C++ Programming
+*/
+
 #include <iostream>
 
 int main()
 {
-	std::cout << "Hello World!";
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "My favorite color is red!";
+	return 0;
 }
